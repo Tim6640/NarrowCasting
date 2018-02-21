b28ee890129b7c1636ad9fa5e6a6d322776d05b8
jpleunis7297 <35992495+jpleunis7297@users.noreply.github.com>
2018-02-21 10:54:14 +0100

    Styling + template aangemaakt.

---
+++ templateO-B.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joey
+ * Date: 21-2-2018
+ * Time: 10:49
+ */
+?>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joey
+ * Date: 20-2-2018
+ * Time: 11:27
+ */
+?>
+<head>
+    <!--Header has yet to be dynamically added during a later stage.-->
+    <!--Inline CSS is used here. Has to be converted to a remote LESS file during a later stage.-->
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet/less" type="text/css" href="assets/less/style.less">
+
+    <title>Template O-B</title>
+
+</head>
+<body>
+
+<div class="container-fluid">
+
+    <div class="row">
+
+        <div id="O" class="col-12">
+
+            <!--Call function here-->
+
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div id="B" class="col-12">
+
+            <!--Call function here-->
+
+        </div>
+
+    </div>
+
+</div>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.0/less.min.js"></script>
+
+</body>
