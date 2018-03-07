8099231aad98d778339fdd2a4c60c6dfa2e51fa8
jpleunis7297 <35992495+jpleunis7297@users.noreply.github.com>
2018-03-07 14:36:14 +0100

    new directory + header.php.
    created a new directory used for includes. created the header.php. header.php is to be included in all templates that are yet to be created.

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joey
+ * Date: 7-3-2018
+ * Time: 14:20
+ */
+?>
+<head>
+    <!--Header has yet to be dynamically added during a later stage.-->
+    <!--Inline CSS is used here. Has to be converted to a remote LESS file during a later stage.-->
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet/less" type="text/css" href="../assets/less/style.less">
+
+    <title>Template L-R</title>
+
+</head>
+<body>
+<div class="header-logo">
+    <img src="../assets/img/Landstede_logo.png" alt="http://www.ondile.nl/magento/media/catalog/category/Landstede_logo_600x240px.png">
+</div>
