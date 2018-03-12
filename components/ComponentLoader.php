65225edb4821f5969b22c32293b87c62ed939c14
jpleunis7297 <35992495+jpleunis7297@users.noreply.github.com>
2018-03-12 09:14:46 +0100

    .

--- components/ComponentLoader.php
+++ components/ComponentLoader.php
@@ -9,7 +9,46 @@
 namespace components;
 
 
-class componentLoader
+class ComponentLoader
 {
+    private $_context;
 
+    private $_isActive;
+
+    public function __construct($context, $isActive) {
+        $this->_context = $context;
+        $this->_isActive = $isActive;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContext()
+    {
+        return $this->_context;
+    }
+
+    /**
+     * @param mixed $context
+     */
+    public function setContext($context)
+    {
+        $this->_context = $context;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsActive()
+    {
+        return $this->_isActive;
+    }
+
+    /**
+     * @param mixed $isActive
+     */
+    public function setIsActive($isActive)
+    {
+        $this->_isActive = $isActive;
+    }
 }
