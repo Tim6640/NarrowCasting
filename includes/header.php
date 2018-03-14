01999e27b2be65e32407fe66955fee0efcfb7d19
jpleunis7297 <35992495+jpleunis7297@users.noreply.github.com>
2018-03-14 11:55:10 +0100

    included styling parsing on header.php (Less.js) so that image is as intended. + styling for said image in style.less.

--- includes/header.php
+++ includes/header.php
@@ -19,3 +19,6 @@
 <div class="header-logo">
     <img src="../assets/img/Landstede_logo.png" alt="http://www.ondile.nl/magento/media/catalog/category/Landstede_logo_600x240px.png">
 </div>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.0/less.min.js"></script>
