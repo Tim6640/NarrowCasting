3ff723a54053f5a10a31722ce3897e8b4c0d5721
Christian <cmakkin6524@student.landstede.nl>
2018-03-21 00:06:27 +0100

    Buienradar iframe working with some styling The img/KNMIgebouw.JPG needs pixresizer

--- components/Buienradar.php
+++ components/Buienradar.php
@@ -17,8 +17,9 @@ class Buienradar extends Crud
 
     //constructor
 
-    public function __construct($urlRadar, $urlGraphs, $latitude, $longitude)
+    public function __construct($urlRadar, $urlGraphs, $latitude, $longitude, $table, $columns, $where='', $whereConditions='', $orderBy='', $value='')
     {
+        parent::__construct($table, $columns, $where, $whereConditions, $orderBy, $value);
         $this->prop_urlRadarView = $urlRadar;
         $this->prop_urlGraphsView = $urlGraphs;
         $this->prop_latitude = $latitude;
@@ -90,19 +91,24 @@ class Buienradar extends Crud
     }
 
     //methods
-    public function getView()
+    public function view()
     {
         echo"
-        <iframe id='mapBuienRadar' src='https://gadgets.buienradar.nl/gadget/zoommap/?lat=52.33575&lng=5.61527&overname=2&zoom=8&naam=3844BD&size=3&voor=1' scrolling=no width=550 height=512 frameborder=no></iframe>
-        <iframe id='graphsBuienRadar' src='//gadgets.buienradar.nl/gadget/forecastandstation/6260/' noresize scrolling=no hspace=0 vspace=0 frameborder=0 marginheight=0 marginwidth=0 width=300 height=190></iframe>
-        
-<script>
-    window.setInterval('reloadIFrame();', 1800000);
-    function reloadIFrame() {
-        document.getElementById('mapBuienRadar').src='https://gadgets.buienradar.nl/gadget/zoommap/?lat=52.33575&lng=5.61527&overname=2&zoom=8&naam=3844BD&size=3&voor=1';
-        document.getElementById('graphsBuienRadar').src='//gadgets.buienradar.nl/gadget/forecastandstation/6260/';
-    }
-</script>
+        <div class='radarCenter'> 
+        <p>
+            <iframe id='mapBuienRadar' src='https://gadgets.buienradar.nl/gadget/zoommap/?lat=52.33575&lng=5.61527&overname=2&zoom=8&naam=3844BD&size=3&voor=1' scrolling=no width=550 height=512 frameborder=no></iframe>
+            <iframe id='graphsBuienRadar' src='//gadgets.buienradar.nl/gadget/forecastandstationportrait/6260/' noresize scrolling=no hspace=0 vspace=0 frameborder=0 marginheight=0 marginwidth=0 width=175 height=518></iframe>
+            <br>
+            Medemogelijk gemaakt door: Buienradar.
+        </p>    
+        </div> 
+        <script>
+            window.setInterval('reloadIFrame();', 1800000);
+            function reloadIFrame() {
+                document.getElementById('mapBuienRadar').src='https://gadgets.buienradar.nl/gadget/zoommap/?lat=52.33575&lng=5.61527&overname=2&zoom=8&naam=3844BD&size=3&voor=1';
+                document.getElementById('graphsBuienRadar').src='//gadgets.buienradar.nl/gadget/forecastandstationportrait/6260/';
+            }
+        </script>
 
         ";
     }
