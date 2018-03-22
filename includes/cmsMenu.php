3cdf3e04eb8863af8006f25fd18fb4cd71f1a048
jpleunis7297 <35992495+jpleunis7297@users.noreply.github.com>
2018-03-22 09:40:10 +0100

    Altered file in order to provide for a better work environment.

--- includes/cmsMenu.php
+++ includes/cmsMenu.php
@@ -5,17 +5,18 @@
  * Date: 16-3-2018
  * Time: 12:07
  */
-?>
-<body>
-    <?php
+
     include_once("autoload.php");
 
     $device = new device();
+
     foreach ($device->selectAllDevices() as $deviceInfo){
-        echo "<a href='cms.php?id=".$deviceInfo['deviceID']."'>".$deviceInfo['deviceName']."</a>";
+
+        echo "<li><a href='cms.php?id=".$deviceInfo['deviceID']."'>".$deviceInfo['deviceName']."</a></li>";
+
         echo "<br>";
+
     }
+
     ?>
 
-    <button>Settings</button>
-</body>
