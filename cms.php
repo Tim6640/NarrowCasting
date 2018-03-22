46c5050ddf9b994be67c90e5082e19bc895abc7c
jpleunis7297 <35992495+jpleunis7297@users.noreply.github.com>
2018-03-22 09:40:40 +0100

    In the midst of building the front-end for the CMS.

--- cms.php
+++ cms.php
@@ -5,66 +5,159 @@
  * Date: 16-3-2018
  * Time: 11:53
  */
-include "includes/cmsMenu.php";
-include "includes/header.php";
+//include "includes/cmsMenu.php";
+//include "includes/header.php";
 
-$device = new device();
+/*$device = new device();
+
 if (isset($_GET['id'])){
+
     $id = $_GET['id'];
+
     foreach($device->getDeviceComponentInfo($id) as $componentInfo){
+
         var_dump($componentInfo);
     }
-}
+}*/
 
-$time = date('H:i:s');
 ?>
+<head>
+    <link rel="stylesheet/less" type="text/css" href="assets/less/style.less">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.0/less.min.js" ></script>
 
-<div class="container-fluid">
+    <title><?php echo $title  ?></title>
 
-    <div class="row">
+</head>
 
-        <div class="col-2">
+<body>
 
-            <h1>Beheer</h1>
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col">
+
+                <h1>Beheer</h1>
+
+                <div class="col-1">
+
+                    <ul>
+
+                        <?php
+
+                        include "includes/cmsMenu.php";
+
+                        ?>
+
+                    </ul>
+
+                    <button>Settings</button>
+
+                </div>
+
+            </div>
+
+            <div class="col-10">
+
+                <div class="row">
+
+                    <h1>Componenten</h1>
+
+                    <h1 id="time"></h1>
+
+                    <p>U bent ingelogd als "[username]" <br><br><a style="float: right;" href="#">Log uit.</a></p>
+
+                </div>
+
+            </div>
 
         </div>
 
-        <div class="col-10">
+        <div class="row">
 
-            <h1>Componenten</h1>
+            <div></div>
 
-            <h1 id="time"></h1>
-
-            <p>U bent ingelogd als "[username]" <a href="#">Log uit.</a></p>
+            <div></div>
 
         </div>
 
     </div>
 
-    <div class="row">
+    <script>
+        /*this code puts the date and time in the header of the CMS dashboard. It is refreshed every second to allow for an exact representation of the time and date.*/
+        setInterval(function () {
 
-        <div></div>
+            var d      = new Date();
+            var hour   = d.getHours();
+            var minute = d.getMinutes();
+            //var second = d.getSeconds();
+            var year   = d.getFullYear();
+            var month  = d.getMonth();
+            var day    = d.getDay();
+            var element;
 
-        <div></div>
+            switch (month) {
+                case 1:
+                    month = "Januari";
+                    break;
+                case 2:
+                    month = "Februari";
+                    break;
+                case 3:
+                    month = "Maart";
+                    break;
+                case 4:
+                    month = "April";
+                    break;
+                case 5:
+                    month = "Mei";
+                    break;
+                case  6:
+                    month = "Juni";
+                    break;
+                case  7:
+                    month = "Juli";
+                    break;
+                case  8:
+                    month = "Augustus";
+                    break;
+                case  9:
+                    month = "September";
+                    break;
+                case  10:
+                    month = "Oktober";
+                    break;
+                case  11:
+                    month = "November";
+                    break;
+                case  12:
+                    month = "December";
+                    break;
+            }
 
-    </div>
+            element = document.getElementById("time");
 
-</div>
+            if (element) {
 
-<script>
+                if( minute < 10 ){
 
-    setInterval(function () {
+                    element.innerHTML = day + " " + month + " " + year + ", " + hour + ":0" + minute;
 
-        var d      = new Date();
-        var hour   = d.getHours();
-        var minute = d.getMinutes();
-        //var second = d.getSeconds();
-        var element;
+                }else{
 
-        element = document.getElementById("time");
-        if (element) {
-            element.innerHTML = hour + ":" + minute;
-        }
-    },1000)
+                    element.innerHTML = day + " " + month + " " + year + ", " + hour + ":" + minute;
 
-</script>
+                    }
+
+                    //element.innerHTML = day + " " + month + " " + year + ", " + hour + ":" + minute;
+
+                }
+
+        },1000)
+
+    </script>
+
+</body>
