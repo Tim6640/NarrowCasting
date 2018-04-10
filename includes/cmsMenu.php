fc98fd6de9a95b26ec7c96ea075f5739aa5c8b65
jpleunis7297 <35992495+jpleunis7297@users.noreply.github.com>
2018-04-10 13:21:04 +0200

    added breadcrumb titles and renamed "gebruikers" to "Beheerders"

--- includes/cmsMenu.php
+++ includes/cmsMenu.php
@@ -40,7 +40,7 @@ switch ($active) {
         <a href="dashboard.php" class="list-group-item <?php echo $dashboard ?>"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Dashboard</a>
         <a href="devices.php" class="list-group-item <?php echo $devices ?>"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Apparaten <span class="badge"><?php echo count($device->getDevices())?></span></a>
         <a href="components.php" class="list-group-item <?php echo $components ?>"><i class="fa fa-puzzle-piece"></i> Componenten <span class="badge"><?php echo count($component->getComponents())?></span></a>
-        <a href="users.php" class="list-group-item <?php echo $users ?>"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Gebruikers <span class="badge"><?php echo count($user->getUsers())?></span></a>
+        <a href="users.php" class="list-group-item <?php echo $users ?>"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Beheerders <span class="badge"><?php echo count($user->getUsers())?></span></a>
         <a href="componentConnect.php" class="list-group-item <?php echo $componentConnect ?>"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Componenten koppelen </a>
         <a href="#" class="list-group-item"> <p>Welkom <?php echo $username ?> </p> <form method="POST">    <input type="submit" value="Log uit" name="logOut"> </form> </a>
     </div>
